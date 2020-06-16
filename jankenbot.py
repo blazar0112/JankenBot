1d4e345d82b0078559dda0063bd923c513e93056
blazar <blazar@PC2020>
2020-06-16 18:47:28 +0800

    Print stamp count.

--- jankenbot.py
+++ jankenbot.py
@@ -20,7 +20,7 @@ class Move(Enum):
     Scissors = 3
 
 # you can change strategy to Strategy.Fixed or Strategy.Random
-strategy = Strategy.Fixed
+strategy = Strategy.Random
 # you can change fixed move, only used when strategy is Strategy.Fixed
 fixed_move = Move.Scissors
 
@@ -32,6 +32,13 @@ japanese_names = { Move.Rock:'グー', Move.Paper:'パー', Move.Scissors:'チ�
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 print('janken page url:'+url)
+
+def print_stamp():
+    try:
+        stamp_element = driver.find_element_by_xpath('//*[@id="inc-janken"]/div/div/p[2]/strong')
+        print('Current stamp =', stamp_element.text+'.')
+    except:
+        print('Cannot find stamp count.')
 
 def check_janken_page():
     print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]'), "Checking janken page...")
@@ -49,13 +56,16 @@ def check_janken_page():
             move = random.choice(list(Move))
         select_element = driver.find_element_by_xpath('//*[@id="janken-select"]/div/a['+str(move.value)+']')
     except:
-        print('Already selected move today.')
+        print('Already selected move.')
+        print_stamp()
         return
 
     select_url = select_element.get_attribute('href')
     print('select_url: ', select_element.get_attribute('href'))
     driver.get(select_url)
     print('Move selected as', move.name, '[JP:', japanese_names[move]+']')
+    driver.get(url)
+    print_stamp()
 
 schedule.every().hour.do(check_janken_page)
 
